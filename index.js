50facc07788787bb982e1ba280efacf063d01e7c
jriv95 <joel.rivera7995@gmail.com>
2019-01-18 07:46:03 -0500

    Fetch might not always be free

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener('DOMContentLoaded', e => {
+
   const burgerMenu = document.querySelector('#burger-menu')
   const orderList = document.querySelector('#order-list')
   const customBurger = document.querySelector('#custom-burger')
@@ -7,6 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
   .then(res => res.json())
   .then(burgers => burgers.forEach(burger => showBurger(burger)))
 
+
   const showBurger = (burger) => {
     burgerMenu.innerHTML += `<div class="burger" data-id="${burger.id}">
       <h3 class="burger_title">${burger.name}</h3>
@@ -14,48 +16,53 @@ document.addEventListener("DOMContentLoaded", () => {
         <p class="burger_description">
           ${burger.description}
         </p>
-        <button class="button" id="button-id">Add to Order</button>
+        <button class="button">Add to Order</button>
     </div>`
   }
 
-burgerMenu.addEventListener("click", e => {
-  if(e.target.classList.contains('button')){
-    const burgerId = e.target.parentNode.getAttribute("data-id")
-    const id = parseInt(burgerId)
-  fetch(`http://localhost:3000/burgers/${id}`)
-  .then(res => res.json())
-  .then(burger => showOrder(burger))
-
-  }
-})
-
-const showOrder = (burger) => {
-  orderList.innerHTML += `<li>${burger.name}</li>`
-}
-
-customBurger.addEventListener("submit", e => {
-  e.preventDefault()
-  const burgerName = document.querySelector('#burger-name').value
-  const burgerDescription = document.querySelector('#burger-description').value
-  const burgerImage = document.querySelector('#burger-image').value
-
-  const burgerData = {
-    name: burgerName,
-    description: burgerDescription,
-    image: burgerImage
-  }
-
-  fetch(`http://localhost:3000/burgers`, {
-    method: 'POST',
-    body: JSON.stringify(burgerData),
-    headers: {
-      "Content-Type": "application/json"
+  burgerMenu.addEventListener('click', e => {
+    if(e.target.classList.contains('button')) {
+      const dataId = e.target.parentNode.getAttribute('data-id')
+      const burgerTitle = e.target.parentNode.querySelector('h3').innerHTML
+      orderList.innerHTML += `<li>${burgerTitle}</li>`
     }
   })
-  .then(res => res.json())
-  .then(burger => showBurger(burger))
 
-})
+// This might not always be free
+  // burgerMenu.addEventListener('click', e => {
+  //   if(e.target.classList.contains('button')) {
+  //       const dataId = e.target.parentNode.getAttribute('data-id')
+  //
+  //       fetch(`http://localhost:3000/burgers/${dataId}`)
+  //       .then(r => r.json())
+  //       .then(burger => orderList.innerHTML += `<li>${burger.name}</li>`)
+  //   }
+  // })
 
 
-}) // END OF DCL
+  customBurger.addEventListener('submit', e => {
+    e.preventDefault()
+    const burgerName = document.querySelector('#burger-name').value
+    const burgerDescription = document.querySelector('#burger-description').value
+    const burgerImage = document.querySelector('#burger-image').value
+
+    burgerData = {
+      name: burgerName,
+      description: burgerDescription,
+      image: burgerImage
+    }
+
+    fetch('http://localhost:3000/burgers', {
+      method: 'POST',
+      body: JSON.stringify(burgerData),
+      headers: {
+        'Content-Type': "application/json"
+      }
+    })
+    .then(r => r.json())
+    .then(burger => showBurger(burger))
+
+  })
+
+
+}) //DOMContentLoaded
